b3d41decde0424a24d0bb4c65e0f11d11a4246de
Yi FU <yvesfu@gmail.com>
2016-10-03 16:38:43 +0200

    add logic structure

--- index.ios.js
+++ index.ios.js
@@ -13,8 +13,43 @@ import {
   View
 } from 'react-native';
 import { Button } from 'react-native-material-design';
+import _ from 'lodash';
 
 class ssp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winCount: 0,
+      straightWinCount: 0,
+      myChoice: null,
+      yourChoice: null,
+      setResult: null,
+      setNumber: 0
+    };
+  }
+
+  /*
+  * 0: scissor, 1: stone, 2: paper
+  *
+  */
+  judge = (myChoice) => (event) => {
+    console.log('judge function called');
+    const yourChoice = _.sample([0,1,2]);
+    console.log(`your choice is: ${yourChoice}`);
+    // TODO: calculate result
+    this.setState({
+      setResult: this.getResult(myChoice, yourChoice)
+    });
+  }
+
+  /*
+  * 0: draw, 1: win, -1: lose
+  *
+  */
+  getResult = (myChoice, yourChoice) => {
+    return 1;
+  }
+
   render() {
     return (
       <View style={styles.container}>
