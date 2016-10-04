6e13a07915883145f6ed7a07cd9207e498506464
Yi FU <yvesfu@gmail.com>
2016-10-04 23:42:05 +0200

    Finish logic

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,8 @@ import {
   StyleSheet,
   Image,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 import { Button } from 'react-native-material-design';
 import _ from 'lodash';
@@ -21,24 +22,74 @@ class ssp extends Component {
     this.state = {
       winCount: 0,
       straightWinCount: 0,
+      highestStraightWinCount: 0,
+      isPreviousWin: false,
       myChoice: null,
       yourChoice: null,
       setResult: null,
-      setNumber: 0
+      setNumber: 0,
+      myImageUri: '',
+      yourImageUri: '',
+      myResultText: '',
+      yourResultText: '',
+      isFirstSet: true
     };
   }
+
+
+  static options = [
+    { name: 'scissor', image: require('./images/scissor.jpg') },
+    { name: 'stone', image: require('./images/stone.jpg') },
+    { name: 'paper', image: require('./images/paper.jpg') }
+  ];
 
   /*
   * 0: scissor, 1: stone, 2: paper
   *
   */
   judge = (myChoice) => (event) => {
-    console.log('judge function called');
     const yourChoice = _.sample([0,1,2]);
-    console.log(`your choice is: ${yourChoice}`);
-    // TODO: calculate result
+    const setResult = this.getResult(myChoice, yourChoice);
+    // Check straight win
+    let straightWinCount = 0;
+    if ((this.state.isPreviousWin || this.state.isFirstSet) && (setResult === 1)) {
+      if (this.state.straightWinCount === 0) {
+        straightWinCount = 2;
+      } else {
+        straightWinCount = this.state.straightWinCount + 1;
+      }
+    }
+    const highestStraightWinCount = straightWinCount > this.state.highestStraightWinCount ? straightWinCount : this.state.highestStraightWinCount;
+
+    let myResultText = '';
+    let yourResultText = '';
+    switch (setResult) {
+      case 1:
+        myResultText = 'win';
+        yourResultText = 'lose';
+        break;
+      case -1:
+        myResultText = 'lose';
+        yourResultText = 'win';
+        break;
+      default:
+        myResultText = 'draw';
+        yourResultText = 'draw';
+
+    }
     this.setState({
-      setResult: this.getResult(myChoice, yourChoice)
+      yourChoice,
+      myChoice,
+      setResult,
+      isPreviousWin: setResult === 1,
+      straightWinCount,
+      highestStraightWinCount,
+      myImageUri: ssp.options[myChoice].image,
+      yourImageUri: ssp.options[yourChoice].image,
+      myResultText,
+      yourResultText,
+      winCount: this.state.winCount + setResult,
+      isFirstSet: false
     });
   }
 
@@ -47,10 +98,18 @@ class ssp extends Component {
   *
   */
   getResult = (myChoice, yourChoice) => {
-    return 1;
+    if (myChoice === yourChoice) {
+      return 0;
+    }
+    if ((myChoice === 0 && yourChoice == 2) || (myChoice === 1 && yourChoice == 0) || (myChoice === 2 && yourChoice == 1)) {
+      return 1;
+    }
+
+    return -1;
   }
 
   render() {
+    const {myImageUri, yourImageUri, myResultText, yourResultText} = this.state;
     return (
       <View style={styles.container}>
         <View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
@@ -80,37 +139,39 @@ class ssp extends Component {
             style={{width: 80, height: 80}}
             source={require('./images/papa_profile.png')}
           />
-        <Text>爸爸</Text>
+          <View style={{flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
+            <Text>爸爸</Text>
+            <Text>{yourResultText}</Text>
+          </View>
           <Image
             style={{width: 80, height: 80}}
-            source={require('./images/stone.jpg')}
+            source={yourImageUri}
           />
         </View>
         <View style={{flex:2, flexDirection: 'row', justifyContent: 'space-around'}}>
-          <Image
-            style={{width: 80, height: 80}}
-            source={require('./images/scissor.jpg')}
-          />
-          <Image
-            style={{width: 80, height: 80}}
-            source={require('./images/stone.jpg')}
-          />
-          <Image
-            style={{width: 80, height: 80}}
-            source={require('./images/paper.jpg')}
-          />
+          { ssp.options.map((option, index) => {
+            return (
+              <TouchableOpacity onPress={this.judge(index)} key={index}>
+                <Image
+                  style={{width: 80, height: 80}}
+                  source={option.image}
+                />
+              </TouchableOpacity>
+            );
+          })}
         </View>
         <View style={{flex:2, flexDirection: 'row', justifyContent: 'space-around'}}>
           <Image
             style={{width: 80, height: 80}}
             source={require('./images/lizi_profile.png')}
           />
-          <Text>
-            李子
-          </Text>
+          <View style={{flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
+            <Text>李子</Text>
+            <Text>{myResultText}</Text>
+          </View>
           <Image
             style={{width: 80, height: 80}}
-            source={require('./images/stone.jpg')}
+            source={myImageUri}
           />
         </View>
         <View style={{flex:1, flexDirection: 'row', alignItems: 'center', backgroundColor: 'skyblue', justifyContent: 'space-around'}}>
