f1e8a9821008898cb375e8e9522db2c3091b7287
Yi FU <yvesfu@gmail.com>
2016-10-06 00:19:16 +0200

    Finish v1

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,8 @@ import {
   Image,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  Modal
 } from 'react-native';
 import { Button } from 'react-native-material-design';
 import _ from 'lodash';
@@ -19,29 +20,51 @@ import _ from 'lodash';
 class ssp extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      winCount: 0,
-      straightWinCount: 0,
-      highestStraightWinCount: 0,
-      isPreviousWin: false,
-      myChoice: null,
-      yourChoice: null,
-      setResult: null,
-      setNumber: 0,
-      myImageUri: '',
-      yourImageUri: '',
-      myResultText: '',
-      yourResultText: '',
-      isFirstSet: true
-    };
+
+    this._resetGame();
   }
 
+  static initialState = {
+   winCount: 0,
+   straightWinCount: 0,
+   highestStraightWinCount: 0,
+   isPreviousWin: false,
+   myChoice: null,
+   yourChoice: null,
+   setResult: null,
+   setNumber: 0,
+   myImageUri: require('./images/default.png'),
+   yourImageUri: require('./images/default.png'),
+   myResultText: '',
+   yourResultText: '',
+   isFirstSet: true,
+   showModal: false,
+   myResultTextStyle: {},
+   yourResultTextStyle: {}
+ };
 
   static options = [
     { name: 'scissor', image: require('./images/scissor.jpg') },
     { name: 'stone', image: require('./images/stone.jpg') },
     { name: 'paper', image: require('./images/paper.jpg') }
   ];
+
+  _setModalVisible = (visible) => {
+    this.setState({showModal: visible});
+  };
+
+  _resetGame = () => {
+    console.log('-----reset called');
+    console.log(this.state);
+    //TODO: Fix reset doesn't work issue
+    this.state = ssp.initialState;
+  }
+
+  _closeModal = () => {
+    // Reset game
+    this._resetGame();
+    this._setModalVisible(false);
+  };
 
   /*
   * 0: scissor, 1: stone, 2: paper
@@ -50,6 +73,7 @@ class ssp extends Component {
   judge = (myChoice) => (event) => {
     const yourChoice = _.sample([0,1,2]);
     const setResult = this.getResult(myChoice, yourChoice);
+    const winCount = (this.state.winCount + setResult) > 0 ? (this.state.winCount + setResult) : 0; // winCount can NOT be less than 0
     // Check straight win
     let straightWinCount = 0;
     if ((this.state.isPreviousWin || this.state.isFirstSet) && (setResult === 1)) {
@@ -63,18 +87,26 @@ class ssp extends Component {
 
     let myResultText = '';
     let yourResultText = '';
+    let myResultTextStyle;
+    let yourResultTextStyle;
     switch (setResult) {
       case 1:
-        myResultText = 'win';
-        yourResultText = 'lose';
+        myResultText = '胜利';
+        myResultTextStyle = styles.winText;
+        yourResultText = '失败';
+        yourResultTextStyle = styles.loseText;
         break;
       case -1:
-        myResultText = 'lose';
-        yourResultText = 'win';
+        myResultText = '失败';
+        myResultTextStyle = styles.loseText;
+        yourResultText = '胜利';
+        yourResultTextStyle = styles.winText;
         break;
       default:
-        myResultText = 'draw';
-        yourResultText = 'draw';
+        myResultText = '平局';
+        myResultTextStyle = styles.drawText;
+        yourResultText = '平局';
+        yourResultTextStyle = styles.drawText;
 
     }
     this.setState({
@@ -88,8 +120,11 @@ class ssp extends Component {
       yourImageUri: ssp.options[yourChoice].image,
       myResultText,
       yourResultText,
-      winCount: this.state.winCount + setResult,
-      isFirstSet: false
+      winCount,
+      isFirstSet: false,
+      showModal: this.state.winCount + setResult === 5,
+      myResultTextStyle,
+      yourResultTextStyle
     });
   }
 
@@ -109,9 +144,10 @@ class ssp extends Component {
   }
 
   render() {
-    const {myImageUri, yourImageUri, myResultText, yourResultText, straightWinCount, winCount} = this.state;
+    const {setResult, myImageUri, yourImageUri, myResultText, yourResultText, straightWinCount, winCount, showModal, myResultTextStyle, yourResultTextStyle} = this.state;
     console.log(`------------------------${straightWinCount}`);
     console.log(`------------${winCount}`);
+
     return (
       <View style={styles.container}>
         <View style={{flex:1, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
@@ -133,8 +169,8 @@ class ssp extends Component {
             source={require('./images/papa_profile.png')}
           />
           <View style={{flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
-            <Text>爸爸</Text>
-            <Text>{yourResultText}</Text>
+            <Text style={styles.nameText}>爸爸</Text>
+            <Text style={yourResultTextStyle}>{yourResultText}</Text>
           </View>
           <Image
             style={{width: 80, height: 80}}
@@ -159,8 +195,8 @@ class ssp extends Component {
             source={require('./images/lizi_profile.png')}
           />
           <View style={{flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
-            <Text>李子</Text>
-            <Text>{myResultText}</Text>
+            <Text style={styles.nameText}>李子</Text>
+            <Text style={myResultTextStyle}>{myResultText}</Text>
           </View>
           <Image
             style={{width: 80, height: 80}}
@@ -168,9 +204,24 @@ class ssp extends Component {
           />
         </View>
         <View style={{flex:1, flexDirection: 'row', alignItems: 'center', backgroundColor: 'skyblue', justifyContent: 'space-around'}}>
-          <Button text='重新开始'></Button>
-          <Button text='结束'></Button>
+          <Button onPress={this._resetGame} text='重新开始'></Button>
+          <Button onPress={this._resetGame} text='结束'></Button>
         </View>
+
+        <Modal
+          animationType={'fade'}
+          transparent={false}
+          visible={this.state.showModal}
+          >
+          <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 200, height: 200}}
+              source={require('./images/smiley_sun_glasses.jpg')}
+            />
+            <Text>恭喜李子小朋友过关!!!</Text>
+            <Button onPress={this._closeModal} text={'知道啦!'} />
+          </View>
+        </Modal>
       </View>
     );
   }
@@ -180,7 +231,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#979797',
-  }
+  },
+  nameText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  winText: {
+    fontSize: 20,
+    color: 'green',
+  },
+  loseText: {
+    fontSize: 20,
+    color: 'red',
+  },
+  drawText: {
+    fontSize: 20,
+    color: 'yellow',
+  },
 });
 
 AppRegistry.registerComponent('ssp', () => ssp);
